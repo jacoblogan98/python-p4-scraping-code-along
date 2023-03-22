dffaef494d4264a91a13883d19808576a4df424a
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-03-22 17:05:03 -0400

    passed tests

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,11 @@
 class Course:
-    pass
+    def __init__(self, title='', schedule='', description='') -> None:
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self) -> str:
+        output = ''
+        output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '------------------'
+        return output
